56905ba75fb5c4c39dabe66390e8eec990bd5a91
Ian Whitlock <ian@theknown.net>
2016-09-23 22:29:59 -0400

    cleanup

--- emacs.d/elpa/dpaste-20120823.1129/dpaste-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dpaste" "20120823.1129" "Emacs integration for dpaste.com [github]" (quote nil))
--- emacs.d/elpa/lcs-20121201.1455/lcs-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; lcs-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("lcs-pkg.el" "lcs.el") (21039 40084 268644
-;;;;;;  70000))
-
-;;;***
-
-(provide 'lcs-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; lcs-autoloads.el ends here
